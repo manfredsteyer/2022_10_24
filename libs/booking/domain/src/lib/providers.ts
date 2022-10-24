f50268f6e58edbd42a59428ce780fb0c68277bf7
Manfred Steyer <manfred.steyer@gmx.net>
2022-10-24 14:33:15 +0200

    update version of ddd plugin

--- libs/booking/domain/src/lib/providers.ts
+++ libs/booking/domain/src/lib/providers.ts
@@ -5,12 +5,13 @@ import { EffectsModule, provideEffects } from "@ngrx/effects";
 import { provideState, StoreModule } from "@ngrx/store";
 import { BookingEffects } from "./+state/effects";
 import { bookingFeature } from "./+state/reducers";
+import { FlightClassEffects } from './+state/flight-class/flight-class.effects';
+import { FLIGHTCLASS_FEATURE_KEY, reducer } from './+state/flight-class/flight-class.reducer';
 
 export function provideBookingDomain() {
     return [
         // importProvidersFrom(StoreModule.forFeature(bookingFeature)),
-        // importProvidersFrom(EffectsModule.forFeature([BookingEffects])),
-        
+        // importProvidersFrom(EffectsModule.forFeature([BookingEffects
         provideState(bookingFeature),
         provideEffects([BookingEffects]),
     ];
