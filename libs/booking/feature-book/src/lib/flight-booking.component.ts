8ee28f03170a9952fe3b0fad2e76227849ff22c3
Manfred Steyer <manfred.steyer@gmx.net>
2022-10-27 22:00:42 +0200

    delete confusing comment

--- libs/booking/feature-book/src/lib/flight-booking.component.ts
+++ libs/booking/feature-book/src/lib/flight-booking.component.ts
@@ -1,8 +1,6 @@
 import { Component } from "@angular/core";
 import { RouterLinkWithHref, RouterOutlet } from "@angular/router";
 
-// import { CheckinDomainModule } from '@nx-example/checkin/domain';
-
 @Component({
   standalone: true,
   selector: 'flight-booking',
