8c7b258f4a0e2310973086ec6a7c2463b6f2e96b
mikezks <23002661+mikezks@users.noreply.github.com>
2022-10-28 09:31:36 +0200

    feat: previous flight filters

--- libs/booking/ui-common/src/lib/flight-filter/flight-filter.component.ts
+++ libs/booking/ui-common/src/lib/flight-filter/flight-filter.component.ts
@@ -1,7 +1,19 @@
 import { CommonModule } from '@angular/common';
 import { Component, EventEmitter, Input, Output } from '@angular/core';
-import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { ComponentStore } from '@ngrx/component-store';
+import { FormBuilder, FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
 import { FlightFilter } from '@nx-example/booking/domain';
+import { map, Observable, tap } from 'rxjs';
+
+
+export interface LocalState {
+  filters: FlightFilter[];
+}
+
+export const initialLocalState: LocalState = {
+  filters: []
+};
+
 
 @Component({
   selector: 'flight-filter',
@@ -12,24 +24,91 @@ import { FlightFilter } from '@nx-example/booking/domain';
   ],
   templateUrl: './flight-filter.component.html',
   styleUrls: ['./flight-filter.component.css'],
+  providers: [
+    ComponentStore
+  ]
 })
 export class FlightFilterComponent {
   @Input() set filter(filter: FlightFilter) {
     this.filterForm.setValue(filter);
   }
-
   @Output() searchTrigger = new EventEmitter<FlightFilter>();
-
   filterForm = this.fb.nonNullable.group({
     from: ['', [Validators.required]],
     to: ['', [Validators.required]],
     urgent: [false]
   });
 
-  constructor(private fb: FormBuilder) {
-  }
+  selectedFilter = new FormControl(this.filterForm.getRawValue(), { nonNullable: true });
 
-  search(): void {
-    this.searchTrigger.next(this.filterForm.getRawValue());
+  /**
+   * Updater
+   */
+
+  addFilter = this.localStore.updater(
+    (state: LocalState, filter: FlightFilter) => ({
+      ...state,
+      filters: [
+        ...state.filters.filter(f => !(
+          f.from === filter.from &&
+          f.to === filter.to
+        )),
+        filter
+      ]
+    })
+  );
+
+  /**
+   * Selectors
+   */
+
+  selectFilters$ = this.localStore.select(
+    // Selectors
+
+    // Projector
+    state => state.filters
+  );
+
+  selectLatestFilters$ = this.localStore.select(
+    // Selectors
+    this.selectFilters$,
+    // Projector
+    filters => filters.slice(-1)[0]
+  );
+
+  /**
+   * Effect
+   */
+
+  updateFilterForm = this.localStore.effect(
+    (filter$: Observable<FlightFilter>) =>
+      filter$.pipe(
+        tap(filter => this.filterForm.patchValue(filter))
+      )
+  );
+
+  updateSelectedFilter = this.localStore.effect(
+    (filter$: Observable<FlightFilter>) =>
+      filter$.pipe(
+        tap((filter: FlightFilter) => this.selectedFilter.setValue(filter))
+      )
+  );
+
+  triggerSearch = this.localStore.effect(
+    (trigger$: Observable<void>) =>
+      trigger$.pipe(
+        map(() => this.filterForm.getRawValue()),
+        tap((filter: FlightFilter) => this.addFilter(filter)),
+        tap((filter: FlightFilter) => this.searchTrigger.next(filter))
+      )
+  );
+
+  constructor(
+    private fb: FormBuilder,
+    private localStore: ComponentStore<LocalState>) {
+
+    this.localStore.setState(initialLocalState);
+    this.updateFilterForm(this.selectedFilter.valueChanges);
+    this.updateSelectedFilter(this.selectLatestFilters$);
   }
 }
