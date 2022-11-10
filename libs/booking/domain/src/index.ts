aaf1f8a62f5b5260d93d38f399c0aad728d6de0c
Rainer Hahnekamp <rainer.hahnekamp@gmail.com>
2022-11-10 15:11:44 +0100

    add create passenger

--- libs/booking/domain/src/index.ts
+++ libs/booking/domain/src/index.ts
@@ -5,4 +5,5 @@ export * from './lib/+state/selectors';
 export * from './lib/providers';
 export * from './lib/entities/flight';
 export * from './lib/entities/passenger';
+export * from './lib/entities/create-passenger';
 export * from './lib/infrastructure/flight.service';
